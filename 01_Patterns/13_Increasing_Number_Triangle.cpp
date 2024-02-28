1caf9b5d94797cf2c3de9e53e5deeb1d3e66d3b1
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-28 07:53:55 +0000

    dsa

---
+++ 01_Patterns/13_Increasing_Number_Triangle.cpp
@@ -0,0 +1,34 @@
+
+ /*
+Input: ‘N’ = 3
+
+Output: 
+
+1
+2 3
+4 5 6
+
+*/
+/*
+Input: ‘N’ = 3
+
+Output: 
+
+1
+2 3
+4 5 6
+
+*/
+void nNumberTriangle(int n) {
+    int number = 1;
+    for(int i = 1 ; i <= n ; i++)
+   {  
+    
+       for(int j =  1 ; j <= i ; j++)
+       {
+            cout<<number<<" ";
+            number++;
+       }
+    cout<<endl;
+    }
+}
