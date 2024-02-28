71e40dd23a258c9d814508b355327a5492d329c8
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-02-28 08:38:45 +0000

    dsa

---
+++ 01_Patterns/15_Alpha_Ramp.cpp
@@ -0,0 +1,23 @@
+/*
+Input: ‘N’ = 3
+
+Output: 
+A
+B B
+C C C
+
+*/
+void alphaRamp(int n) {
+    for(int i = 0 ; i < n ; i++)
+    
+    {
+        char ch  = 'A'+i;
+        for(int j = 0 ; j <= i ;j ++)
+        {
+            
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+    
+}
