4415996f14b1e9c569f93e78c68ab392fe9f84b0
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-03-01 22:39:24 +0530

    Update 02-Forest.cpp

--- 01_Patterns/02-Forest.cpp
+++ 01_Patterns/02-Forest.cpp
@@ -1,10 +1,18 @@
+Example:
+Input:  ‘N’ = 3
+
+Output: 
+* 
+* *
+* * *
+
 void nForest(int n) {
-for (int i = 0 ; i < n ; i ++)
-{
+    for (int i = 0 ; i < n ; i ++)
+    {
 	for(int j = 0 ; j <= i ;j++)
 	{
 		cout<<"*"<<" ";
 	}
 	cout<<endl;
-}
+     }
 }
