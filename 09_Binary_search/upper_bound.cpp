1b24be214056ca939c9bdee5219850be2ce7e995
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-03-09 15:07:39 +0000

    dsa

---
+++ 09_Binary_search/upper_bound.cpp
@@ -0,0 +1,60 @@
+
+
+//brute force 
+int upperBound(vector<int> &arr, int x, int n){
+   for(int i = 0 ; i < n ; i ++)
+    {
+	     if(arr[i]>x)
+	    {
+		return i;
+		break;
+	    }
+   }
+return n;	
+}
+/*
+Time complexity : O(n)
+Space Complexity : O(1)
+*/
+
+
+//optimal solution : binary search 
+
+
+
+//brute force 
+int upperBound(vector<int> &arr, int x, int n){
+   int start = 0;
+   int end = n -1;
+   int mid ;
+   int ans= n;
+   while(start<=end)
+   {
+	   mid = start + ( end - start)/ 2;
+	   if(arr[mid]> x)
+	   {
+		   ans=mid;
+		   end = mid - 1;
+
+	   }
+	   else
+	   {
+		   start = mid + 1;
+	   }
+	   
+   }
+   return ans;
+}
+/*
+Time Complexity: O(logN), where N = size of the given array.
+Reason: We are basically using the Binary Search algorithm.
+
+Space Complexity: O(1) as we are using no extra space.
+*/
+
+
+//using stl
+
+int upperBound(vector<int> &arr, int x, int n){
+	return upper_bound(arr.begin(), arr.end(), x)-arr.begin();	
+}
