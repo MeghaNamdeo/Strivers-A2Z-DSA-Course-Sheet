c7f2db1cf6166f46d9e64cb5412671f60558b510
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-03-10 16:34:57 +0000

    dsa

--- 07_Array/intersection.cpp
+++ 07_Array/intersection.cpp
@@ -1,3 +1,30 @@
+//brute force 
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = 0; j < nums2.size(); j++) {
+                if (nums1[i] == nums2[j]) {
+                    ans.push_back(nums1[i]);
+                }
+            }
+        }
+
+       
+
+        set<int> uniqueSet(ans.begin(), ans.end());
+        vector<int> uniqueVector(uniqueSet.begin(), uniqueSet.end());
+        return uniqueVector;
+    }
+};
+/*
+Time Complexity: O(N + M) - The loop iterates through both nums1 and nums2 once.
+
+Space Complexity: O(min(N, M)) - The space used is proportional to the smaller of the two input arrays.
+*/
+
+//better approach 
 int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
      set<int>st;
       for(int i =0 ; i < n;i++)
