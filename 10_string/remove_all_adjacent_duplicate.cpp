7352fdb39589f7fe6c0fb7a71eeb19962028207c
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-03-14 21:00:05 +0000

    dsa

--- 10_string/remove_all_adjacent_duplicate.cpp
+++ 10_string/remove_all_adjacent_duplicate.cpp
@@ -30,4 +30,32 @@ public:
         
     }
 };
-/* Time complexity : O(n), Space complexity : O(n)
+/* Time complexity : O(n), Space complexity : O(n)*/
+
+//or
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans ="";
+        int i =0;
+
+        while(i<s.length())
+        {
+
+                if(ans.length()>0&&ans[ans.length()-1]==s[i])
+                {
+                    ans.pop_back();
+                }
+                else
+                {
+                    ans.push_back(s[i]);
+                }
+            
+            i++;
+            
+        }
+        return ans;
+        
+    }
+};
+/* Time complexity : O(n), Space complexity : O(n)*/
