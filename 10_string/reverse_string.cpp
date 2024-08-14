b5965becb8d87b952642a4f49615f1afff0c1f40
MeghaNamdeo <96281329+MeghaNamdeo@users.noreply.github.com>
2024-08-14 17:36:01 +0530

    Update reverse_string.cpp

--- 10_string/reverse_string.cpp
+++ 10_string/reverse_string.cpp
@@ -27,4 +27,24 @@ for(;start<=end;)
 }
 return str;
 }
+
+
+//using stack
+#include <bits/stdc++.h> 
+string reverseString(string str)
+{
+	stack<char>st;
+	for(int i =0;i<str.length();i++)
+	{
+		st.push(str[i]);
+	}
+	string ans ="";
+	while(!st.empty())
+	{
+		ans = ans + st.top();
+		st.pop();
+	}
+	return ans;
+}
+/* TC :O(n) SC : O(n)*/
 */
